a89d7ba9538a6a1e512aa9244c4cc7744851fbd5
Анур Ниматов <anurnimatov20@gmail.com>
2026-07-16 15:06:36 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,13 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/test/start', 'TestController@start')->name('test.start')->middleware('auth');
 Route::get('/run-migrations', function () {
+    // Ogohlantirishlarni yashirish
+    error_reporting(E_ERROR | E_PARSE);
+    
     try {
         \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-        return "Muvaffaqiyatli! Jadvallar yaratildi:<br><pre>" . \Illuminate\Support\Facades\Artisan::output() . "</pre>";
+        return "Natija: <br>" . \Illuminate\Support\Facades\Artisan::output();
     } catch (\Exception $e) {
-        return "Xatolik yuz berdi: " . $e->getMessage();
+        return "Xatolik: " . $e->getMessage();
     }
 });
